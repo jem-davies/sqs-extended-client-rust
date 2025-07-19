fd523b646e632dcdeb075a39d0c52b23766e9965
Jem Davies <jemsot@gmail.com>
2025-07-19 23:43:12 +0100

    create all fields on client struct
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,81 @@
 use aws_sdk_s3;
 use aws_sdk_sqs;
 
+const MAX_MESSAGE_SIZE_IN_BYTES: u64 = 262144; // TODO - why is this a u64 when it's a literal const of 262,144?
+static DEFAULT_POINTER_CLASS: &str = "software.amazon.payloadoffloading.PayloadS3Pointer";
+
 pub struct SqsExtendedClient {
-    s3_client: aws_sdk_s3::Client,
-    sqs_client: aws_sdk_sqs::Client,
-    always_s3: bool
+    s3_client: aws_sdk_s3::Client,      //
+    sqs_client: aws_sdk_sqs::Client,    //
+    bucket_name: String,                //
+    message_size_threshold: u64,        //
+    batch_messages_size_threshold: u64, //
+    always_via_s3: bool,                //
+    pointer_class: String,              //
+    //reserved_attributes: []String, // TODO
+    object_prefix: String,
+    //base_s3_pointer_size: i32, // TODO int -> what should the default size golang's int be?
+    //base_attribute_size: i32,  // TODO int -> what should the default size golang's int be?
 }
 
 impl SqsExtendedClient {
-    // pub fn new(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client, always_s3: bool) -> SqsExtendedClient {
-    //     SqsExtendedClient{
-    //         s3_client,
-    //         sqs_client,
-    //         always_s3,
-    //     }
-    // }
-
-    pub fn builder(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client) -> SqsExtendedClientBuilder {
-        SqsExtendedClientBuilder::new(s3_client, sqs_client)
+    pub fn builder(
+        s3_client: aws_sdk_s3::Client,
+        sqs_client: aws_sdk_sqs::Client,
+        bucket_name: String,
+    ) -> SqsExtendedClientBuilder {
+        SqsExtendedClientBuilder::new(s3_client, sqs_client, bucket_name)
     }
 
     pub fn send_message(&self) -> String {
+        // TODO implement
         "Sent Message!".to_string()
     }
 
     pub fn always_s3_message(&self) -> String {
-        "Always S3 is: ".to_string() + &self.always_s3.to_string()
+        // TODO delete
+        "Always S3 is: ".to_string() + &self.always_via_s3.to_string()
     }
 }
 
 pub struct SqsExtendedClientBuilder {
     s3_client: aws_sdk_s3::Client,
     sqs_client: aws_sdk_sqs::Client,
-    always_s3: bool
+    bucket_name: String,
+    always_s3: bool,
+    object_prefix: String,
 }
 
 impl SqsExtendedClientBuilder {
-    pub fn new(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client) -> SqsExtendedClientBuilder {
-        SqsExtendedClientBuilder{
+    pub fn new(
+        s3_client: aws_sdk_s3::Client,
+        sqs_client: aws_sdk_sqs::Client,
+        bucket_name: String,
+    ) -> SqsExtendedClientBuilder {
+        SqsExtendedClientBuilder {
             s3_client,
             sqs_client,
-            always_s3: false
+            bucket_name,
+            always_s3: false,
+            object_prefix: "".to_string(),
         }
     }
 
-    pub fn always_s3(mut self) -> SqsExtendedClientBuilder {
+    pub fn always_via_s3(mut self) -> SqsExtendedClientBuilder {
         self.always_s3 = true;
         self
     }
 
     pub fn build(self) -> SqsExtendedClient {
-        SqsExtendedClient {s3_client: self.s3_client, sqs_client: self.sqs_client, always_s3: self.always_s3}
+        SqsExtendedClient {
+            s3_client: self.s3_client,
+            sqs_client: self.sqs_client,
+            bucket_name: self.bucket_name,
+            message_size_threshold: MAX_MESSAGE_SIZE_IN_BYTES,
+            batch_messages_size_threshold: MAX_MESSAGE_SIZE_IN_BYTES,
+            always_via_s3: self.always_s3,
+            pointer_class: DEFAULT_POINTER_CLASS.to_string(),
+            object_prefix: self.object_prefix,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use sqs_extended_client_rust::{SqsExtendedClient, SqsExtendedClientBuilder};
 
 #[::tokio::main]
 async fn main() {
-    let config: aws_config::SdkConfig = aws_config::load_defaults(BehaviorVersion::v2025_01_17()).await;
+    let config: aws_config::SdkConfig =
+        aws_config::load_defaults(BehaviorVersion::v2025_01_17()).await;
     let s3_client: aws_sdk_s3::Client = aws_sdk_s3::Client::new(&config);
     let sqs_client: aws_sdk_sqs::Client = aws_sdk_sqs::Client::new(&config);
 
-    let sqs_extended_client: SqsExtendedClient = SqsExtendedClientBuilder::new(s3_client, sqs_client).build();
+    let sqs_extended_client: SqsExtendedClient =
+        SqsExtendedClientBuilder::new(s3_client, sqs_client, "bucket_name".to_string())
+            .always_via_s3()
+            .build();
 
     println!("{}", sqs_extended_client.send_message());
     println!("{}", sqs_extended_client.always_s3_message());
