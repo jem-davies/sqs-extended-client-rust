78d232bb1ee391cae411785cb6dc1878d7eb67cc
Jem Davies <jemsot@gmail.com>
2025-07-19 22:04:16 +0100

    implement builder pattern for always_s3
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,55 @@
-pub struct SqsExtendedClient {}
+use aws_sdk_s3;
+use aws_sdk_sqs;
+
+pub struct SqsExtendedClient {
+    s3_client: aws_sdk_s3::Client,
+    sqs_client: aws_sdk_sqs::Client,
+    always_s3: bool
+}
 
 impl SqsExtendedClient {
-    pub fn new() -> SqsExtendedClient {
-        println!("Hello, from lib.rs!");
-        SqsExtendedClient{}
+    // pub fn new(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client, always_s3: bool) -> SqsExtendedClient {
+    //     SqsExtendedClient{
+    //         s3_client,
+    //         sqs_client,
+    //         always_s3,
+    //     }
+    // }
+
+    pub fn builder(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client) -> SqsExtendedClientBuilder {
+        SqsExtendedClientBuilder::new(s3_client, sqs_client)
+    }
+
+    pub fn send_message(&self) -> String {
+        "Sent Message!".to_string()
+    }
+
+    pub fn always_s3_message(&self) -> String {
+        "Always S3 is: ".to_string() + &self.always_s3.to_string()
+    }
+}
+
+pub struct SqsExtendedClientBuilder {
+    s3_client: aws_sdk_s3::Client,
+    sqs_client: aws_sdk_sqs::Client,
+    always_s3: bool
+}
+
+impl SqsExtendedClientBuilder {
+    pub fn new(s3_client: aws_sdk_s3::Client, sqs_client: aws_sdk_sqs::Client) -> SqsExtendedClientBuilder {
+        SqsExtendedClientBuilder{
+            s3_client,
+            sqs_client,
+            always_s3: false
+        }
+    }
+
+    pub fn always_s3(mut self) -> SqsExtendedClientBuilder {
+        self.always_s3 = true;
+        self
+    }
+
+    pub fn build(self) -> SqsExtendedClient {
+        SqsExtendedClient {s3_client: self.s3_client, sqs_client: self.sqs_client, always_s3: self.always_s3}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use sqs_extended_client_rust::SqsExtendedClient;
+use aws_config::BehaviorVersion;
+use sqs_extended_client_rust::{SqsExtendedClient, SqsExtendedClientBuilder};
 
-fn main() {
-    SqsExtendedClient::new();
+#[::tokio::main]
+async fn main() {
+    let config: aws_config::SdkConfig = aws_config::load_defaults(BehaviorVersion::v2025_01_17()).await;
+    let s3_client: aws_sdk_s3::Client = aws_sdk_s3::Client::new(&config);
+    let sqs_client: aws_sdk_sqs::Client = aws_sdk_sqs::Client::new(&config);
+
+    let sqs_extended_client: SqsExtendedClient = SqsExtendedClientBuilder::new(s3_client, sqs_client).build();
+
+    println!("{}", sqs_extended_client.send_message());
+    println!("{}", sqs_extended_client.always_s3_message());
 }
