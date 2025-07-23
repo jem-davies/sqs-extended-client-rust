3ea3e3d983b18a17baf87a7e78f0a8f19c203f18
Jem Davies <jemsot@gmail.com>
2025-07-23 22:48:56 +0100

    working fn send_message
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,32 @@
+use core::panic;
+use std::collections::HashMap;
+
 use aws_sdk_s3;
 use aws_sdk_sqs;
+use aws_sdk_sqs::operation::send_message::builders::SendMessageFluentBuilder;
+use aws_sdk_sqs::operation::send_message::{SendMessageError, SendMessageOutput};
+use aws_sdk_sqs::types::MessageAttributeValue;
+use aws_smithy_runtime_api::client::orchestrator::HttpResponse;
+use aws_smithy_runtime_api::client::result::SdkError;
+use aws_smithy_types::byte_stream::ByteStream;
+use uuid::Uuid;
 
-const MAX_MESSAGE_SIZE_IN_BYTES: u64 = 262144; // TODO - why is this a u64 when it's a literal const of 262,144?
+const MAX_MESSAGE_SIZE_IN_BYTES: usize = 262144; // TODO - why is this a u64 when it's a literal const of 262,144?
 static DEFAULT_POINTER_CLASS: &str = "software.amazon.payloadoffloading.PayloadS3Pointer"; // TODO - should this be a static? 
 
 pub struct SqsExtendedClient {
     s3_client: aws_sdk_s3::Client,   //
     sqs_client: aws_sdk_sqs::Client, //
     // logger here in Go implementation
-    bucket_name: String,                //
-    message_size_threshold: u64,        //
-    batch_messages_size_threshold: u64, //
-    always_through_s3: bool,            //
-    pointer_class: String,              //
-    reserved_attributes: Vec<String>,   //
-    object_prefix: String,
-    base_s3_pointer_size: i32, // TODO int -> what should the default size golang's int be?
-    base_attribute_size: i32,  // TODO int -> what should the default size golang's int be?
+    bucket_name: String,                  //
+    message_size_threshold: usize,        //
+    batch_messages_size_threshold: usize, //
+    always_through_s3: bool,              //
+    pointer_class: String,                //
+    reserved_attributes: Vec<String>,     //
+    object_prefix: String,                //
+    base_s3_pointer_size: usize, // TODO usize -> what should the default size golang's int be?
+    base_attribute_size: usize,  // TODO int -> what should the default size golang's int be?
 }
 
 impl SqsExtendedClient {
@@ -27,13 +37,95 @@ impl SqsExtendedClient {
         SqsExtendedClientBuilder::new(s3_client, sqs_client)
     }
 
-    pub fn send_message(&self) -> String {
-        // TODO implement
-        "Sent Message!".to_string()
+    pub async fn send_message(
+        &self,
+        msg_input: SendMessageFluentBuilder,
+    ) -> Result<SendMessageOutput, SdkError<SendMessageError, HttpResponse>> {
+        println!("{}", "Sending message!");
+        if self.bucket_name == "" {
+            panic!("you gotta set bucket_name for send message bro")
+        }
+
+        if self.always_through_s3
+            || self.message_exceeds_threshold(
+                msg_input.get_message_body().clone(), // TODO remove the clone here - send by reference!
+                msg_input.get_message_attributes().clone(), // TODO remove the clone here - send by reference!
+            )
+        {
+            // generate s3 object key
+            let s3_key: String = Uuid::new_v4().to_string(); // TODO add prefix
+
+            // upload large payload to s3
+            let _ = self
+                .s3_client
+                .put_object()
+                .bucket(self.bucket_name.clone())
+                .key(s3_key.clone())
+                .body(self.convert_string_byte_stream(msg_input.get_message_body().clone()))
+                .send()
+                .await;
+
+            // create an s3 pointer that will be uploaded to SQS in place of the large payload
+            let new_msg: S3Pointer = S3Pointer {
+                s3_bucket_name: self.bucket_name.clone(),
+                s3_key: s3_key,
+                class: self.pointer_class.clone(),
+            };
+
+            // copy over all attributes, leaving space for our reserved attribute
+
+            // assign the reserved attribute to a number containing the size of the original body
+
+            // override attributes and body in the original message (make a copy)
+            msg_input.message_body(new_msg.marshall_json()).send().await
+        } else {
+            msg_input.send().await
+        }
     }
 
-    pub fn receive_message(&self) {
-        panic!("NOT IMPLEMENTED")
+    pub fn receive_message(&self) -> String {
+        // TODO implement
+        "base_s3_pointer_size: ".to_string() + &self.base_s3_pointer_size.to_string()
+    }
+
+    fn message_exceeds_threshold(
+        &self,
+        body: Option<String>,
+        attributes: Option<HashMap<String, MessageAttributeValue>>,
+    ) -> bool {
+        match body {
+            None => false, // TODO -> error return
+            Some(b) => self.message_size(b, attributes).total() > self.message_size_threshold,
+        }
+    }
+
+    fn message_size(
+        &self,
+        body: String,
+        attributes: Option<HashMap<String, MessageAttributeValue>>,
+    ) -> MessageSize {
+        MessageSize {
+            body_size: body.len(),
+            attribute_size: self.attribute_size(attributes),
+        }
+    }
+
+    fn attribute_size(&self, attributes: Option<HashMap<String, MessageAttributeValue>>) -> usize {
+        match attributes {
+            None => return 0,
+            Some(hash_map) => return self.calc_attribute_size(hash_map),
+        }
+    }
+
+    fn calc_attribute_size(&self, attributes: HashMap<String, MessageAttributeValue>) -> usize {
+        88
+    }
+
+    fn convert_string_byte_stream(&self, s: Option<String>) -> ByteStream {
+        match s {
+            None => panic!("HHENOCKE"),
+            Some(s) => ByteStream::from(s.into_bytes()),
+        }
     }
 }
 
@@ -41,8 +133,8 @@ pub struct SqsExtendedClientBuilder {
     s3_client: aws_sdk_s3::Client,
     sqs_client: aws_sdk_sqs::Client,
     bucket_name: String,
-    message_size_threshold: u64,
-    batch_message_size_threshold: u64,
+    message_size_threshold: usize,
+    batch_message_size_threshold: usize,
     always_s3: bool,
     pointer_class: String,
     reserved_attributes: Vec<String>,
@@ -76,14 +168,14 @@ impl SqsExtendedClientBuilder {
         self
     }
 
-    pub fn with_message_size_threshold(mut self, msg_size: u64) -> SqsExtendedClientBuilder {
+    pub fn with_message_size_threshold(mut self, msg_size: usize) -> SqsExtendedClientBuilder {
         self.message_size_threshold = msg_size;
         self
     }
 
     pub fn with_batch_message_size_threshold(
         mut self,
-        batch_msg_size: u64,
+        batch_msg_size: usize,
     ) -> SqsExtendedClientBuilder {
         self.batch_message_size_threshold = batch_msg_size;
         self
@@ -113,7 +205,13 @@ impl SqsExtendedClientBuilder {
     }
 
     pub fn build(self) -> SqsExtendedClient {
-        // TODO set base_s3_pointer_size & base_attribute_size
+        let ptr: S3Pointer = S3Pointer {
+            s3_bucket_name: String::from(""), // bucket name is blank here
+            s3_key: Uuid::new_v4().to_string(),
+            class: self.pointer_class.clone(),
+        };
+
+        // TODO base_attribute_size
 
         SqsExtendedClient {
             s3_client: self.s3_client,
@@ -125,11 +223,43 @@ impl SqsExtendedClientBuilder {
             pointer_class: self.pointer_class,
             reserved_attributes: self.reserved_attributes,
             object_prefix: self.object_prefix,
-            base_s3_pointer_size: 0, // Go default/zero value here?
-            base_attribute_size: 0,  // Go default/zero value here?
+            base_s3_pointer_size: ptr.marshall_json().len(),
+            base_attribute_size: 0,
         }
     }
 }
+
+//------------------------------------------------------------------------------
+
+struct S3Pointer {
+    s3_bucket_name: String,
+    s3_key: String,
+    class: String,
+}
+
+impl S3Pointer {
+    fn marshall_json(self) -> String {
+        String::from(format!(
+            "[\"{}\",{{\"s3BucketName\":\"{}\",\"s3Key\":\"{}\"}}]",
+            self.class, self.s3_bucket_name, self.s3_key
+        ))
+    }
+}
+
+//------------------------------------------------------------------------------
+
+struct MessageSize {
+    body_size: usize,
+    attribute_size: usize,
+}
+
+impl MessageSize {
+    fn total(self) -> usize {
+        self.body_size + self.attribute_size
+    }
+}
+
+//------------------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
@@ -188,6 +318,7 @@ mod tests {
         );
         assert_eq!("pointer-class", sqs_extended_client.pointer_class);
         assert_eq!("object-prefix", sqs_extended_client.object_prefix);
+        assert_eq!(84, sqs_extended_client.base_s3_pointer_size);
     }
 
     #[test]
@@ -211,5 +342,6 @@ mod tests {
         );
         assert_eq!(DEFAULT_POINTER_CLASS, sqs_extended_client.pointer_class);
         assert_eq!("", sqs_extended_client.object_prefix);
+        assert_eq!(121, sqs_extended_client.base_s3_pointer_size);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use aws_config::BehaviorVersion;
+use aws_sdk_sqs::operation::send_message::builders::SendMessageFluentBuilder;
 use sqs_extended_client_rust::{SqsExtendedClient, SqsExtendedClientBuilder};
 
 #[::tokio::main]
-async fn main() {
+async fn main() -> Result<(), aws_sdk_sqs::Error> {
+    // what should this return be? -> guess it won't matter because this will go anyway...
     let config: aws_config::SdkConfig =
         aws_config::load_defaults(BehaviorVersion::v2025_01_17()).await;
     let s3_client: aws_sdk_s3::Client = aws_sdk_s3::Client::new(&config);
     let sqs_client: aws_sdk_sqs::Client = aws_sdk_sqs::Client::new(&config);
+    let clone_sqs_client: aws_sdk_sqs::Client = sqs_client.clone();
 
     let sqs_extended_client: SqsExtendedClient =
         SqsExtendedClientBuilder::new(s3_client, sqs_client)
-            .with_s3_bucket_name("bucket_name".to_string())
-            .with_always_through_s3(true)
-            .with_message_size_threshold(20)
+            .with_s3_bucket_name("".to_string())
+            .with_always_through_s3(false)
+            .with_message_size_threshold(200000)
             .build();
 
-    println!("{}", sqs_extended_client.send_message());
+    let msg_input: SendMessageFluentBuilder = clone_sqs_client
+        .send_message()
+        .queue_url("")
+        .message_body("HELLO SQS FROM RUST! :)");
+
+    println!("{}", "HELLO FROM MAIN");
+
+    sqs_extended_client.send_message(msg_input).await?;
+    Ok(())
 }
